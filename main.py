58495af36968a28683dc71d0fb2c0d74815fa724
amiruldayev <114305101+amiruldayev@users.noreply.github.com>
2023-10-07 11:46:53 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+print("\t\t\t\t\tEven numbers")
+def even_numbers():
+    try:
+        n = int(input())
+        a = 1
+        while a<n:
+            a += 1
+            if a%2==0:
+                print(a)
+
+    except:
+        print()
+
+
+def  Factorial():
+    try:
+        n = int(input())
+        a = 1
+        res = 1
+        while a<=n:
+            res *= a
+            a += 1
+        print(res)
+
+    except:
+        print("Error!")
+
+
+def Number_search():
+    array = []
+    try:
+        if(array.append()
+        array.append(1)
+
+    except:
+        print("Error!")
+
+#even_numbers()
+#Factorial()
+Number_search()
